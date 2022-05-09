a0f2f14e0063e376a6497c58841131c5c386c934
kamil289@vp.pl <kamil289@vp.pl>
2022-05-09 21:47:05 +0200

    organization layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { OrganizationGetComponent } from './components/organization-get/organization-get.component';
+import { MainComponent } from './components/main/main.component';
+import { OrganizationAddComponent } from './components/organization-add/organization-add.component';
+import { OrganizationEditComponent } from './components/organization-edit/organization-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrganizationGetComponent,
+    MainComponent,
+    OrganizationAddComponent,
+    OrganizationEditComponent
   ],
   imports: [
     BrowserModule
